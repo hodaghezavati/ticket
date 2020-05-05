ffb641b268e3eb4df79ccb775295f883d8d76eec
hodaghezavati <hodaghezavati@gmail.com>
2020-05-05 19:44:54 +0430

    first commit

--- config/app.php
+++ config/app.php
@@ -162,7 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
